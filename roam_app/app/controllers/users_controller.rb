66553c48f9629d2c04ab4a8816f698c7dc70a314
Erin Green <erin.y.green@gmail.com>
2019-04-09 16:32:48 +0000

    Adds user validations

--- roam_app/app/controllers/users_controller.rb
+++ roam_app/app/controllers/users_controller.rb
@@ -5,10 +5,30 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
 
+    if user.valid? && params[:user][:password] == params[:user][:password_confirmation]
+        @user = User.create(user_params)
+        session[:user_id] = @user.id
+        redirect_to ("/users/#{@user.id}")
+    else
+      flash[:notice] = "Please complete the form with valid entries."
+      redirect_to(controller: 'users', action: 'new')
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    
   end
 
   def update
 
   end
+
+  private
+
+   def user_params
+     params.require(:user).permit(:name, :email, :bio, :ago, :password, :password_confirmation)
+   end
 end
--- roam_app/app/models/user.rb
+++ roam_app/app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_many :trips
   has_many :activities, through: :trips
-  has_many :creations, class_name: :Activity, foreign_key: :creator_id 
+  has_many :creations, class_name: :Activity, foreign_key: :creator_id
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :age, :numericality => { :greater_than_or_equal_to => 0}
+  validates :password, presence: true
 
 end
