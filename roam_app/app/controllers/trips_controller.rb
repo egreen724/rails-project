d59c03ccb498e7a0991946bbe5ffa2b22942d307
Erin <erin.y.green@gmail.com>
2019-04-13 13:13:55 -0400

    Creates trip show page and fixes forms

--- roam_app/app/controllers/trips_controller.rb
+++ roam_app/app/controllers/trips_controller.rb
@@ -6,11 +6,13 @@ class TripsController < ApplicationController
   end
 
   def create
+    @trip = Trip.create(trip_params)
+    redirect_to ("/trips/#{@trip.id}") #add nested routes
 
   end
 
   def show
-
+    @trip = Trip.find(params[:id])
   end
 
   def trip_params
@@ -18,8 +20,8 @@ class TripsController < ApplicationController
       :activity_id,
       :user_id,
       :date,
-      :time,
-      :comments,
+      :time_ellapsed,
+      :comment,
       :taken
     )
   end
