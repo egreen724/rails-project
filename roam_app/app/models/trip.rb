a00552d03c045d786a2f9a94d13fb3b9dfd3c5f1
Erin <erin.y.green@gmail.com>
2019-04-13 13:25:31 -0400

    Removes mins_toHours method

--- roam_app/app/models/trip.rb
+++ roam_app/app/models/trip.rb
@@ -2,8 +2,7 @@ class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :activity
 
-  def mins_to_hours
-    minutes = params[:trip][:minutes]
+  def mins_to_hours(minutes)
     if minutes > 60
       minutes/60
     else
