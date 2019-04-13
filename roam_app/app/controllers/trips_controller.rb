b55ea244ce73a18ba461c501b0bdaf4e4d2a9f12
Erin <erin.y.green@gmail.com>
2019-04-13 12:37:43 -0400

    Creates trip form

--- roam_app/app/controllers/trips_controller.rb
+++ roam_app/app/controllers/trips_controller.rb
@@ -1,16 +1,27 @@
 class TripsController < ApplicationController
 
   def new
-
+    @trip = Trip.new
+    @current_user = current_user
   end
 
   def create
 
   end
-  
+
   def show
 
   end
 
+  def trip_params
+    params.require(:trip).permit(
+      :activity_id,
+      :user_id,
+      :date,
+      :time,
+      :comments,
+      :taken
+    )
+  end
 
 end
