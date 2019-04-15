642d710954d936371f9a603a951fb313a1db0ffd
Erin <erin.y.green@gmail.com>
2019-04-15 11:13:05 -0400

    Adding filter functionality

--- roam_app/app/models/activity.rb
+++ roam_app/app/models/activity.rb
@@ -6,11 +6,54 @@ class Activity < ApplicationRecord
   has_many :keywords, through: :activity_keywords
   accepts_nested_attributes_for :keywords
 
+  validates :distance, presence: true, :numericality => { :greater_than_or_equal_to => 0}
+  validates :state, presence: true
+
+
+  def self.state_filter(state_input)
+    where(state: state_input)
+  end
+
+  def self.category_filter(category_input)
+    where(category: category_input)
+  end
+
+  def self.greater_than_ten
+    where("distance >= 10")
+  end
+
+  def self.five_to_ten
+    where("distance >= 5 AND distance < 10")
+  end
+
+  def self.two_to_five
+    where("distance >= 2 AND distance < 5")
+  end
+
+  def self.greater_than_ten
+    where("distance < 2")
+  end
+
   def keywords_attributes=(keyword_attributes)
     keyword_attributes.values.each do |keyword_attribute|
       keyword = Keyword.find_or_create_by(keyword_attribute)
       self.keywords << keyword
     end
   end
+
+  def comments
+    self.trips.each do |trip|
+     if trip.comment != nil
+       self.comments << trip.comment
+     end
+   end
+   self.comments
+  end
+
+  # <% @activity.trips.each do |trip| %> </h2>
+  #   <ul>
+  #     <li> <%= trip.comment unless trip.comment == nil %> </li>
+  #   </ul>
+  #   <%end%>
 
 end
